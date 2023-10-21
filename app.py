5356627b118226973573b8ab1517d437d2bff40f
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-21 18:34:51 -0400

    prueba

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 from Flask_sqlalchemy import SQLAlchemy
 from os import environ
 
@@ -118,3 +118,7 @@ def partial_update_user(id):
     return make_response(jsonify({'message': 'user not found'}), 404)
   except Exception as e:
     return make_response(jsonify({'message': 'error updating user'}), 500)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
