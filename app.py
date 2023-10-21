64104e07c337cf72187b1ed0632ed8e2af838b44
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-21 08:18:11 -0400

    configurando

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_cors import CORS
+
+from config import config
+
+# Routes
+from routes import Movie
 
 app = Flask(__name__)
 
-# Ruta de ejemplo
-@app.route('/')
-def hello_world():
-    return jsonify(message='Hola, mundo Flask!')
+CORS(app, resources={"*": {"origins": "http://localhost:9300"}})
+
+
+def page_not_found(error):
+    return "<h1>Not found page</h1>", 404
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.config.from_object(config['development'])
+
+    # Blueprints
+    app.register_blueprint(Movie.main, url_prefix='/api/movies')
+
+    # Error handlers
+    app.register_error_handler(404, page_not_found)
+    app.run()
