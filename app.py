964feabcaa09b50a48fed2b1467d1a73442b4dd6
Karina González <129917232+karinaagnzlz@users.noreply.github.com>
2023-10-21 00:13:39 -0400

    Creating containers

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-# Ruta de ejemplo
-@app.route('/')
-def hello_world():
-    return jsonify(message='Hola, mundo Flask!')
+# Ruta de ejemplo utilizando Flask-RESTful
+class HelloWorld(Resource):
+    def get(self):
+        return {'message': 'Hola, mundo Flask-RESTful!'}
+
+api.add_resource(HelloWorld, '/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
