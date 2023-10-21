9be097fdaebc1982345ce48cf85368cdce92322b
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-21 00:30:41 -0400

    Vacio

--- Practica2C_Grupo5/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.postgres.fields import ArrayField
-
-
-class Directory(models.Model):
-
-    name = models.CharField(max_length=500, null=False)
-
-    emails = ArrayField(models.EmailField(), null=False)
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /opt/Practica2C_Grupo5/venv/bin/activate
-export DJANGO_SETTINGS_MODULE="Practica2C_Grupo5.settings"
-export GUNICORN_CMD_ARGS='--bind=0.0.0.0 --access-logfile="-"'
-django-admin migrate
-exec gunicorn Practica2C_Grupo5.wsgi
