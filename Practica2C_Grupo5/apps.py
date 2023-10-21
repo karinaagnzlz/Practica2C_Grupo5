6f69f0aadfb6bd4edd157b9b977eb9175ebb0bd8
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-20 22:44:34 -0400

    prueba 2

---
+++ Practica2C_Grupo5/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CnP2SimpleWs2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Practica2C_Grupo5'
--- runtests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose start && docker-compose run --rm -e BASE_URL=web:8000 web behave $@
