f7e3b3f0bd7bd09bce92310107a6455be9f45e96
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-20 23:25:38 -0400

    Prueba 3

--- features/note/delete.feature
+++
@@ -1,6 +0,0 @@
-Feature: Delete note
-
-    Scenario: Delete note
-        Given I set header "Content-Type" with value "application/json"
-        When I send a DELETE request to "/notes/469ba781e138232929c00afa"
-        Then the response code should be 204
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+def get_version():
+    with open("cn_p2_simple_ws/__init__.py") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1])
+
+
+def get_requires():
+    requirements_list = []
+    with open("requirements.txt") as f:
+        for line in f:
+            requirements_list.append(line)
+    return requirements_list
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="cn_p2_simple_ws",
+    version=get_version(),
+    description="cn_p2_simple_ws",
+    long_description=readme,
+    author="Rómulo Rodríguez",
+    packages=find_packages(),
+    install_requires=get_requires(),
+    include_package_data=True,
+)
