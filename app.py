136a52e898b5ad93e0d824a3b9606af6d8a755c3
Jose El Asmar <jose-elasmar1999@outlook.com>
2023-10-21 21:20:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = 'users'
+@app.route('/books', methods=['GET'])
+def get_books():
+    return jsonify(jsonify({'response': 'pong'}))
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def json(self):
-        return {'id': self.id,'username': self.username, 'email': self.email}
-
-db.create_all()
+@app.route('/your-api/')
+def empty_view(self):
+    content = {'your content here'}
+    return content, status.HTTP_201_CREATED
 
 # creando el status
 @app.route('/status', methods=['GET'])
